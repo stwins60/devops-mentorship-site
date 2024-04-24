d899de4597696687048959e23c298f9475fa3421
Idris Fagbemi <idrisniyi94@gmail.com>
2024-04-24 00:37:07 -0500

    Modified code

--- app.py
+++ app.py
@@ -54,56 +54,56 @@ def course_search():
     selected_course = "Linux"
     html_content = ""
     
-    if request.method == 'POST':
-        selected_course = request.form.get('search-term')
-        if selected_course == "Linux":
-            lab_number = request.form.get('lab-number')
-            BASE_URL = "https://raw.githubusercontent.com/livialima/linuxupskillchallenge/master/docs/"
-            response = requests.get(f"{BASE_URL}{lab_number}.md")
-            if response.status_code == 200:
-                content = response.text
-                html_content = markdown.markdown(content)
+    try:
+        if request.method == 'POST':
+            selected_course = request.form.get('search-term')
+            if selected_course == "Linux":
+                lab_number = request.form.get('lab-number')
+                BASE_URL = "https://raw.githubusercontent.com/livialima/linuxupskillchallenge/master/docs/"
+                response = requests.get(f"{BASE_URL}{lab_number}.md")
+                if response.status_code == 200:
+                    content = response.text
+                    html_content = markdown.markdown(content)
+                else:
+                    result = "Course not found"
+            elif selected_course == "Bash Scripting":
+                if isinstance(shellscript, dict):
+                    lab_number = request.form.get('lab-number')
+                    question = shellscript.get(lab_number, "Lab not found").get('question', "Lab not found")
+                    html_content = markdown.markdown(question)
+                else:
+                    html_content = markdown.markdown("<h1>Lab not found</h1>")
+
+            elif selected_course == "Python":
+                lab_number = request.form.get('lab-number')
+                question = python.get(lab_number, "Lab not found").get('question', "Lab not found")
+                html_content = markdown.markdown(question)
+            
+            elif selected_course == "Docker":
+                lab_number = request.form.get('lab-number')
+                question = docker.get(lab_number, "Lab not found").get('question', "Lab not found")
+                html_content = markdown.markdown(question)
+            
+            elif selected_course == "Kubernetes":
+                lab_number = request.form.get('lab-number')
+                question = kubernetes.get(lab_number, "Lab not found").get('question', "Lab not found")
+                html_content = markdown.markdown(question)
+            
+            elif selected_course == "AWS":
+                lab_number = request.form.get('lab-number')
+                question = aws.get(lab_number, "Lab not found").get('question', "Lab not found")
+                html_content = markdown.markdown(question)
+            
+            elif selected_course == "Terraform":
+                lab_number = request.form.get('lab-number')
+                question = terraform.get(lab_number, "Lab not found").get('question', "Lab not found")
+                html_content = markdown.markdown(question)
+            
             else:
                 result = "Course not found"
-        elif selected_course == "Bash Scripting":
-            if isinstance(shellscript, dict):
-                lab_number = request.form.get('lab-number')
-                question = shellscript.get(lab_number, "Lab not found").get('question', "Lab not found")
-                html_content = markdown.markdown(question)
-    #             print(lab_number)
-    #             for key, value in shellscript.items():
-    #                 # print(key, value)
-    #                 if key == lab_number:
-    #                     # print(key, value)
-    #                     course = value
-    #                     html_content = markdown.markdown(course)
-        elif selected_course == "Python":
-            lab_number = request.form.get('lab-number')
-            question = python.get(lab_number, "Lab not found").get('question', "Lab not found")
-            html_content = markdown.markdown(question)
-        
-        elif selected_course == "Docker":
-            lab_number = request.form.get('lab-number')
-            question = docker.get(lab_number, "Lab not found").get('question', "Lab not found")
-            html_content = markdown.markdown(question)
-        
-        elif selected_course == "Kubernetes":
-            lab_number = request.form.get('lab-number')
-            question = kubernetes.get(lab_number, "Lab not found").get('question', "Lab not found")
-            html_content = markdown.markdown(question)
-        
-        elif selected_course == "AWS":
-            lab_number = request.form.get('lab-number')
-            question = aws.get(lab_number, "Lab not found").get('question', "Lab not found")
-            html_content = markdown.markdown(question)
-        
-        elif selected_course == "Terraform":
-            lab_number = request.form.get('lab-number')
-            question = terraform.get(lab_number, "Lab not found").get('question', "Lab not found")
-            html_content = markdown.markdown(question)
-        
-        else:
-            result = "Course not found"
+    except Exception as e:
+        print(e)
+        html_content = markdown.markdown("<h1>Lab not found</h1>")
       
           
     return render_template('course_search.html', choices=choices, selected_course=selected_course, html_content=html_content)
